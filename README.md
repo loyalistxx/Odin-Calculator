af3384fe02406bf6f9cd0affbd189c335ef904cc
adham <jokersuckurmum@gmail.com>
2022-11-27 02:21:11 +0200

    Finished Project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Odin-Calculator
+
+A Simple Calculator made for The Odin Project made by Yours truly. [Live Demo](loyalistxx.github,io/Odin-Calculator/)
+
+## What i used
+
+HTML, CSS And a Jumble of JavaScript
+
+## What did i learn from this experience
+
+Pain, but an expectation of the pain to come, which lessens it a bit
--- javascript.js
+++ javascript.js
@@ -1,100 +1,152 @@
-const add = (num1, num2) => {
-  return num1 + num2;
-};
+class Calculator {
+  constructor(prevOperandTextElement, currentOperandTextElement) {
+    this.prevOperandTextElement = prevOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
 
-const subtract = (num1, num2) => {
-  return num1 - num2;
-};
+  clear() {
+    this.prevOperand = "";
+    this.currentOperand = "";
+    this.operation = undefined;
+  }
 
-const multiply = (num1, num2) => {
-  return num1 * num2;
-};
+  backspace() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+  keyboardInput(e) {
+    if ((e.key >= 0 && e.key <= 9) || e.key == ".") this.appendNumber(e.key);
+    if (e.key === "=" || e.key === "Enter") this.operate(e.key);
+    if (e.key === "Backspace") this.backspace();
+    if (e.key === "Escape") this.clear();
+    if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+      this.appendOperation(this.converter(e.key));
+  }
+  converter(key) {
+    if (key === "/") return "÷";
+    if (key === "*") return "×";
+    if (key === "-") return "-";
+    if (key === "+") return "+";
+  }
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
 
-const divide = (num1, num2) => {
-  return num1 / num2;
-};
+  appendOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.prevOperand !== "") {
+      this.operate();
+    }
+    this.operation = operation;
+    this.prevOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
 
-const operate = (operator, num1, num2) => {
-  operator(num1, num2);
-};
-
-const plusButton = document.querySelector("#plus");
-const minusButton = document.querySelector("#minus");
-const multiplyButton = document.querySelector("#multiply");
-const divideButton = document.querySelector("#divide");
-
-let calcResult = document.querySelector("#calculatorResult");
-
-const one = document.querySelector("#one");
-const two = document.querySelector("#two");
-const three = document.querySelector("#three");
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
-const dot = document.querySelector("#dot");
-const zero = document.querySelector("#zero");
-
-const clear = document.querySelector("#clear");
-const backspace = document.querySelector("#backspace");
-
-plusButton.addEventListener("click", () => {
-    if (calcResult.textContent == null) {
+  operate() {
+    let computation;
+    const prev = parseFloat(this.prevOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = add(prev, current);
+        break;
+      case "-":
+        computation = subtract(prev, current);
+        break;
+      case "×":
+        computation = multiply(prev, current);
+        break;
+      case "÷":
+        computation = divide(prev, current);
+        break;
+      default:
         return;
-    } else if ()
-});
-minusButton.addEventListener("click", () => {
-  calcResult.textContent += " - ";
-});
-multiplyButton.addEventListener("click", () => {
-  calcResult.textContent += " * ";
-});
-divideButton.addEventListener("click", () => {
-  calcResult.textContent += " ÷ ";
-});
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.prevOperand = "";
+  }
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 1,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation != null) {
+      this.prevOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.prevOperand
+      )} ${this.operation}`;
+    } else {
+      this.prevOperandTextElement.innerText = "";
+    }
+  }
+}
 
-one.addEventListener("click", () => {
-  calcResult.textContent += "1";
-});
-two.addEventListener("click", () => {
-  calcResult.textContent += "2";
-});
-three.addEventListener("click", () => {
-  calcResult.textContent += "3";
-});
-four.addEventListener("click", () => {
-  calcResult.textContent += "4";
-});
-five.addEventListener("click", () => {
-  calcResult.textContent += "5";
-});
-six.addEventListener("click", () => {
-  calcResult.textContent += "6";
-});
-seven.addEventListener("click", () => {
-  calcResult.textContent += "7";
-});
-eight.addEventListener("click", () => {
-  calcResult.textContent += "8";
-});
-nine.addEventListener("click", () => {
-  calcResult.textContent += "9";
-});
-dot.addEventListener("click", () => {
-  calcResult.textContent += ".";
-});
-zero.addEventListener("click", () => {
-  calcResult.textContent += "0";
-});
+const prevOperandTextElement = document.querySelector("[data-prevOperand]");
+const currentOperandTextElement = document.querySelector(
+  "[data-currentOperand]"
+);
 
-clear.addEventListener("click", () => {
-  calcResult.textContent = null;
+const operationButtons = document.querySelectorAll("[data-operation]");
+const numberButtons = document.querySelectorAll("[data-number]");
+const equalButton = document.querySelector("[data-equal]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const backspaceButton = document.querySelector("[data-backspace]");
+
+const calculator = new Calculator(
+  prevOperandTextElement,
+  currentOperandTextElement
+);
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+window.addEventListener("keydown", (e) => {
+  calculator.keyboardInput(e);
+  calculator.updateDisplay();
 });
-backspace.addEventListener("click", () => {
-  calcResult.textContent = calcResult.textContent.substring(
-    0,
-    calcResult.textContent.length - 1
-  );
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+equalButton.addEventListener("click", (button) => {
+  calculator.operate();
+  calculator.updateDisplay();
+});
+allClearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+backspaceButton.addEventListener("click", (button) => {
+  if (currentOperand == null) return;
+  calculator.backspace();
+  calculator.updateDisplay();
 });
